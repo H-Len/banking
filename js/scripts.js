700be7f171c668b8f28412174cb1f1e391bce9df
Holly DeBelser and Hannah Melendy <holly@>
2019-06-18 15:10:54 -0700

    Add deposit button and withdraw button

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
+  var currentBal = "";
   console.log ("Jquery Loaded");
   $("#welcome").click(function(event) {
     event.preventDefault();
       var userName = $("#name").val();
       console.log(userName);
-      var initialDeposit = parseInt($("#initialDeposit").val());
+      var initialDeposit = parseFloat($("#initialDeposit").val());
       console.log(initialDeposit);
-    });
-  });
+      currentBal = initialDeposit;
+      console.log(currentBal);
+      $("#currentBal").text(currentBal);
+      // $("#currentBal").text(initialDeposit);
+  })
+
+
+  $("#deposit").click(function(event) {
+    event.preventDefault();
+      var deposit = parseFloat($("#depositInput").val());
+      console.log(deposit);
+      currentBal += deposit;
+      console.log(currentBal);
+      $("#currentBal").text(currentBal);
+  })
+  $("#withdraw").click(function(event) {
+    event.preventDefault();
+      var withdraw = parseFloat($("#withdrawInput").val());
+      console.log(withdraw);
+      currentBal -= withdraw;
+      console.log(currentBal);
+      $("#currentBal").text(currentBal);
+  })
+
+});
